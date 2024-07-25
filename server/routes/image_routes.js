64eac52151d7adb5e70c874288cfbd64c11b60b9
Technical73 <w50790679@gmail.com>
2024-07-25 09:00:47 +0530

    completed

--- server/routes/image_routes.js
+++ server/routes/image_routes.js
@@ -111,10 +111,6 @@ router.get("/getimages", async (req, res) => {
   }
 });
 
-router.get("/", (req, res) => {
-  res.status.json({ message: "hello" });
-});
-
 router.get("/upload/getimage/:id", async (req, res) => {
   try {
     const { id } = req.params;
